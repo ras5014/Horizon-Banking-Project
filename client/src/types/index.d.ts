c9bed1913dc94a6b8ca301bcc8ac0f4e68a1f9c6
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2025-05-23 19:11:09 +0530

    feat: add TotalBalanceBox and AnimateCounter components; update package dependencies

--- client/src/types/index.d.ts
+++ client/src/types/index.d.ts
@@ -1,6 +1,25 @@
+declare type Account = {
+  id: string;
+  availableBalance: number;
+  currentBalance: number;
+  officialName: string;
+  mask: string;
+  institutionId: string;
+  name: string;
+  type: string;
+  subtype: string;
+  appwriteItemId: string;
+  sharableId: string;
+};
 declare interface HeaderBoxProps {
   type: "title" | "greeting";
   title: string;
   subtext: string;
   user?: string;
+}
+
+declare interface TotalBalanceBoxProps {
+  accounts: Account[];
+  totalBanks: number;
+  totalCurrentBalance: number;
 }
